b07db538946388c730e8692075f740098a94c9ed
mikaem <mkmanaligod@gmail.com>
2024-09-29 18:58:38 -0700

    add util function to format text file when displaying

--- readWriteFile/app.js
+++ readWriteFile/app.js
@@ -3,7 +3,7 @@ const {
     URL
 } = require('url');
 const {
-    handleError
+    handleError, formatTextFile
 } = require('./modules/utils.js');
 var fs = require('fs');
 const port = process.env.PORT || 3000;
@@ -23,21 +23,39 @@ http.createServer(function (req, res) {
                     'Content-Type': 'text/html'
                 })
             }
-            res.write(`File updated`);
+            res.write(`File updated. "${text}" appended to file.`);
             res.end();
         })
-    }
-    if (myURL.pathname === ("/read")) {
-        fs.readFile(path, function (err, data) {
+    } else if (myURL.pathname === ("/read")) {
+        fs.readFile(path, function (err, text) {
+            const formattedText = formatTextFile(text)
             if (err) {
                 handleError(res, '500', err.message)
             } else {
                 res.writeHead(200, {
                     'Content-Type': 'text/html'
                 });
-                res.write(data);
+                res.write(formattedText);
                 return res.end();
             }
         })
+    } else {
+        res.writeHead(200, {
+            'Content-Type': 'text/html'
+        });
+        res.write(`
+                        <h2>Enter Text to Add to the File</h2>
+            <form action="/" method="GET">
+                <label for="text">Text to Add:</label>
+                <input type="text" id="text" name="text" required>
+                <button type="submit">Submit</button>
+            </form>
+            <br>
+            <p>Or use the following endpoints:</p>
+            <ul>
+                <li><strong>/?text=YourTextHere</strong>: Appends the specified text to the file <code>text.txt</code>.</li>
+                <li><strong><a href="/read">/read</a></strong>: Reads the contents of the file <code>text.txt</code> and displays it on the screen.</li>
+            </ul>
+`)
     }
 }).listen(port);
