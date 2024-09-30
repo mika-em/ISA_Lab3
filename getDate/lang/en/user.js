9e526a451815a44f5f2591bc2f398c63ec34ffa1
mikaem <mkmanaligod@gmail.com>
2024-09-29 19:46:36 -0700

    added a type module

--- getDate/lang/en/user.js
+++ getDate/lang/en/user.js
@@ -1,1 +1,2 @@
-export var message = "Hello %1, what a beautiful day. Server current date and time is: "
+var message = "Hello %1, what a beautiful day. Server current date and time is: ";
+module.exports = { message };
